795f3c22b142c6dd4ebbcb01763ca439b87b39f9
littlephillips <phillipkinuthia72@gmail.com>
2022-11-07 22:28:22 +0300

    web works

---
+++ how-web-works.rb
@@ -0,0 +1,46 @@
+# define client and server
+# define a static site vs a dynamic site
+# explain what a HTTP request is
+# explain the nature of request and response
+
+#when working on the server, our goal is always:
+  # :: Process a request from a client
+  # :: create a response
+  # :: send the response to the client
+
+
+#HTTP 
+  # :: Hyper-Text Transfer Protocol 
+
+#Requests
+  # :: URL - uniform resource locator
+# https://github.com/learn-co-curriculum/phase-3-how-the-web-works-readme
+  # :: https - the protocol , format we use to send our requests
+  # :: github.com - the domain name
+  # :: /learn-co-curriculum/phase-3-how-the-web-works-readme - the path
+
+#HTTP VERB
+  # :: GET - retrieves a representation of a resource
+  # :: POST - create a new resource using data n the bosy of the request
+  # :: PUT - update an existing resource using data in the bosy of the request
+  # :: PATCH - update part of an existing resource using data in the body of the request
+  # :: DELETE - delete a specific resource
+  # :: HEAD - asks for a response (like GET but without the body)
+  # :: Trace - echoes back the received request
+  # :: OPTIONS - returns the HTTP methods the server supports
+  # :: CONNECT - converts the request to a TCP/IP tunnel (generally for ssl)
+
+#Request Format
+  # :: has request headers / metadata
+
+#Response
+  # :: separated into headers & the body
+  # :: :: Headers
+  # :: :: :: request method
+  # :: :: :: status code
+  # :: :: :: etcetra || metadata of the repsonse
+  # :: :: Body 
+  # :: :: :: HTML/CSS
+
+
+
